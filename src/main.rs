15ec9fc09ae1289ef24d5f5e636b3f364759a48e
Barbatruc <6886536+barbatrucs@users.noreply.github.com>
2025-10-31 21:09:35 +0100

    return the nth Fibonacci number for user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21...
+
+    let mut input = String::new();
+
+    println!("--- Welcome to the Fibonacci number generator --- \n");
+    println!(" please enter a positive integer");
+
+    // get user input
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let input: u32 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Please provide an unassigned integer"),
+    };
+
+    // find the corresponding fibonacci number & give feedback to the user
+    println!(
+        "The {}th Fibonacci number is {}",
+        input,
+        nth_fibonacci(input)
+    );
+}
+
+fn nth_fibonacci(n: u32) -> u32 {
+    // base case : if n = 0 or n = 1 then return n
+    if n <= 1 {
+        return n;
+    }
+
+    // recursive case : sum of the 2 preceding Fibonacci numbers
+    return nth_fibonacci(n - 1) + nth_fibonacci(n - 2);
 }
