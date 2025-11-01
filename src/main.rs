b2ac7397dd83bad1bc769f87e5ae2dbc38d477d9
Barbatruc <6886536+barbatrucs@users.noreply.github.com>
2025-11-01 21:07:11 +0100

    loop to get several input and refactor for iterative computation versus recursive one

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,48 @@
 use std::io;
 
 fn main() {
-    // The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21...
-
-    let mut input = String::new();
 
     println!("--- Welcome to the Fibonacci number generator --- \n");
-    println!(" please enter a positive integer");
 
-    // get user input
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
+    loop {
+        let mut input = String::new();
 
-    let input: u32 = match input.trim().parse() {
-        Ok(num) => num,
-        Err(_) => panic!("Please provide an unassigned integer"),
-    };
+        println!(" please enter a positive integer or type \"quit\" to exit the program");
 
-    // find the corresponding fibonacci number & give feedback to the user
-    println!(
-        "The {}th Fibonacci number is {}",
-        input,
-        nth_fibonacci(input)
-    );
+        // get user input
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        if input.trim() == "quit" {
+            break;
+        }
+
+        let input: u64 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => panic!("Please provide an unassigned integer"),
+        };
+
+        // find the corresponding fibonacci number & give feedback to the user
+        println!(
+            "The {}th Fibonacci number is {}",
+            input,
+            nth_fibonacci(input)
+        );
+    }
 }
 
-fn nth_fibonacci(n: u32) -> u32 {
-    // base case : if n = 0 or n = 1 then return n
-    if n <= 1 {
-        return n;
+fn nth_fibonacci(n: u64) -> u64 {
+    let mut a = 1;
+    let mut b = 0;
+    let mut count = 0;
+
+    while count < n {
+        let temp = a + b;
+        b = a;
+        a = temp;
+        count += 1;
     }
 
-    // recursive case : sum of the 2 preceding Fibonacci numbers
-    return nth_fibonacci(n - 1) + nth_fibonacci(n - 2);
+    b
 }
